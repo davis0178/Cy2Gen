e55adfa983e766b6d62154632dd90dd08fe0fe57
Davis <565295725@qq.com>
2025-04-15 14:00:30 -0400

    Delete Cy2Gen directory

--- Cy2Gen/main.py
+++
@@ -1,8 +0,0 @@
-import os
-import subprocess
-import webbrowser
-
-# start jupyter notebook server
-notebook_path = os.path.abspath("notebooks/generate_cy2.ipynb")
-
-subprocess.Popen(["jupyter", "notebook", notebook_path])
